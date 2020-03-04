fda584cd2507e42e2cb8ff4feeeac8e2e8fabaf0
Nico Virgino <nicovirgino@gmail.com>
2020-03-04 14:54:57 -0600

    test commit for spring

---
+++ src/main/java/com/navirgino/navirginopersonalwebsite/NavirginoPersonalWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.navirgino.navirginopersonalwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavirginoPersonalWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NavirginoPersonalWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/navirgino/navirginopersonalwebsite/NavirginoPersonalWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navirgino.navirginopersonalwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavirginoPersonalWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
