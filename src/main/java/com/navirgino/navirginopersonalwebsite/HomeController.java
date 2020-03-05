50b77b0ff40648c6284f72f5761495f23b1dbac8
Nico Virgino <nicovirgino@gmail.com>
2020-03-05 11:41:24 -0600

    added react modules using npm

--- src/main/java/com/navirgino/navirginopersonalwebsite/HomeController.java
+++ src/main/java/com/navirgino/navirginopersonalwebsite/HomeController.java
@@ -3,12 +3,13 @@ package com.navirgino.navirginopersonalwebsite;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String GetSplashPageView() {
-        return "views/splash";
+        return "views/index";
     }
 
 }
